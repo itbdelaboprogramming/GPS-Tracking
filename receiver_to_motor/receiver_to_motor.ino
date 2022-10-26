7a6fd8a6272b29a58c7ce8b6250b5876d4c4d4c9
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2022-10-26 14:29:56 +0700

    Menambahkan failsafe channel 3 untuk RC

--- receiver_to_motor/receiver_to_motor.ino
+++ receiver_to_motor/receiver_to_motor.ino
@@ -9,29 +9,38 @@
 **/
 
 // Pin untuk baca receiver adalah pin digital biasa
-#define PIN_CH_1 12 
-#define PIN_CH_2 13
+#define PIN_CH_1 48
+#define PIN_CH_2 50
+#define PIN_CH_3 52
 // Channel 1 untuk maju-mundur (move)
 // Channel 2 untuk kiri-kanan (turn)
 
 // Pin untuk enable motor driver
-#define PIN_EN 8
+#define PIN_EN 40
 
 // Pin RPWM dan LPWM untuk motor kiri dan kanan
-#define PIN_RPWM_RIGHT 5
-#define PIN_LPWM_RIGHT 6
-#define PIN_RPWM_LEFT 9
-#define PIN_LPWM_LEFT 10
+#define PIN_RPWM_RIGHT 12
+#define PIN_LPWM_RIGHT 13
+#define PIN_RPWM_LEFT 10
+#define PIN_LPWM_LEFT 11
 
 // Nilai max pwm maju
 #define MAX_PWM_MOVE 63 // Kecepatan max(255)/4
 #define MAX_PWM_TURN 31 // Kecepatan max(255)/8
 #define MOTOR_THRESHOLD 5 // Batas diam motor 5/255
+#define PWM_THRESHOLD 50
 
 int ch1; // output channel 1
 int ch2; // output channel 2
+int ch3; // output channel 3
 int moveValue; // nilai pwm gerakan maju-mundur
 int turnValue; // nilai pwm gerakan kiri-kanan
+
+int forwardValue;
+int backwardValue;
+int rightValue;
+int leftValue;
+
 int pwmRight; // pwm motor kanan
 int pwmLeft; // pwm motor kiri
 
@@ -39,6 +48,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(PIN_CH_1, INPUT);
   pinMode(PIN_CH_2, INPUT);
+  pinMode(PIN_CH_3, INPUT);
 
   pinMode(PIN_EN, OUTPUT);
   pinMode(PIN_RPWM_RIGHT, OUTPUT);
@@ -56,44 +66,74 @@ void loop() {
   // Menerima sinyal pwm dari receiver
   ch1 = pulseIn(PIN_CH_1, HIGH, 1000000);
   ch2 = pulseIn(PIN_CH_2, HIGH, 1000000);
+  ch3 = pulseIn(PIN_CH_3, HIGH, 1000000);
 
   // Membatasi nilai pwm yang terbaca
   ch1 = constrain(ch1, 1000, 2000);
   ch2 = constrain(ch2, 1000, 2000);
+  ch3 = constrain(ch3, 1000, 2000);
 
   // Mengkonversi nilai 1000-2000 menjadi rentang nilai pwm yg diinginkan
-  moveValue = map(ch1, 1000, 2000, -MAX_PWM_MOVE, MAX_PWM_MOVE);
-  turnValue = map(ch2, 1000, 2000, -MAX_PWM_TURN, MAX_PWM_TURN);
+  //moveValue = map(ch1, 1000, 2000, -MAX_PWM_MOVE, MAX_PWM_MOVE);
+  //turnValue = map(ch2, 1000, 2000, -MAX_PWM_TURN, MAX_PWM_TURN);
+
+  if(ch1 >= 1500+PWM_THRESHOLD){
+    moveValue = map(ch1, 1500+PWM_THRESHOLD, 2000, 0, MAX_PWM_MOVE);
+  } else if(ch1 <= 1500-PWM_THRESHOLD){
+    moveValue = map(ch1, 1500-PWM_THRESHOLD, 1000, 0, -MAX_PWM_MOVE);
+  } else {
+    moveValue = 0;
+  }
+
+  if(ch2 >= 1500+PWM_THRESHOLD){
+    turnValue = map(ch2, 1500+PWM_THRESHOLD, 2000, 0, MAX_PWM_TURN);
+  } else if(ch2 <= 1500-PWM_THRESHOLD){
+    turnValue = map(ch2, 1500-PWM_THRESHOLD, 1000, 0, -MAX_PWM_TURN);
+  } else {
+    turnValue = 0;
+  }
 
   /*
    *            PWM
    *        Kanan   Kiri
    * Move   +pwm    +pwm
-   * Turn   +pwm    -pwm
+   * Turn   -pwm    +pwm
    * 
    */
 
-  
-  pwmRight = moveValue + turnValue;
-  pwmLeft = moveValue - turnValue;
+  pwmRight = moveValue - turnValue;
+  pwmLeft = moveValue + turnValue;
 
-  motor_right(pwmRight);
-  motor_left(pwmLeft);
+  if(ch3 <= 1250){
+    Serial.print("Mode Hold");
+  } else if(ch3 >= 1750){
+    Serial.print("Mode Auto");
+  } else {
+    Serial.print("Mode Manual");
+    motor_right(pwmRight);
+    motor_left(pwmLeft);
+  }
 
-  Serial.print(pwmRight);Serial.print("\t");
-  Serial.print(pwmLeft);Serial.print("\t");
-  //Serial.print(ch1);Serial.print("\t");
-  //Serial.print(ch2);Serial.print("\t");
+  //motor_right(pwmRight);
+  //motor_left(pwmLeft);
+
+  //Serial.print(pwmRight);Serial.print("\t");
+  //Serial.print(pwmLeft);Serial.print("\t");
+  //Serial.print(moveValue);Serial.print("\t");
+  //Serial.print(turnValue);Serial.print("\t");
+  Serial.print(ch1);Serial.print("\t");
+  Serial.print(ch2);Serial.print("\t");
+  //Serial.print(ch3);Serial.print("\t");
   Serial.println();
 }
 
 void motor_right(int pwm){
-  if(pwm >= MOTOR_THRESHOLD){
-    analogWrite(PIN_RPWM_RIGHT, pwm);
-    analogWrite(PIN_LPWM_RIGHT, 0);
-  } else if(pwm <= -MOTOR_THRESHOLD){
+  if(pwm > 0){
     analogWrite(PIN_RPWM_RIGHT, 0);
-    analogWrite(PIN_LPWM_RIGHT, -pwm);
+    analogWrite(PIN_LPWM_RIGHT, pwm);
+  } else if(pwm < -0){
+    analogWrite(PIN_RPWM_RIGHT, -pwm);
+    analogWrite(PIN_LPWM_RIGHT, 0);
   } else {
     analogWrite(PIN_RPWM_RIGHT, 0);
     analogWrite(PIN_LPWM_RIGHT, 0);
@@ -101,15 +141,14 @@ void motor_right(int pwm){
 }
 
 void motor_left(int pwm){
-  if(pwm >= MOTOR_THRESHOLD){
-    analogWrite(PIN_RPWM_LEFT, 0);
-    analogWrite(PIN_LPWM_LEFT, pwm);
-  } else if(pwm <= -MOTOR_THRESHOLD){
-    analogWrite(PIN_RPWM_LEFT, -pwm);
+  if(pwm > 0){
+    analogWrite(PIN_RPWM_LEFT, pwm);
     analogWrite(PIN_LPWM_LEFT, 0);
+  } else if(pwm < 0){
+    analogWrite(PIN_RPWM_LEFT, 0);
+    analogWrite(PIN_LPWM_LEFT, -pwm);
   } else {
     analogWrite(PIN_RPWM_LEFT, 0);
     analogWrite(PIN_LPWM_LEFT, 0);
   }
 }
-
