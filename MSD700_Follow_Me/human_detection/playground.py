ee884cc4a3eddee94b169f7a062dd86e38086891
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-24 17:41:57 +0700

    Add playground

--- MSD700_Follow_Me/human_detection/playground.py
+++ MSD700_Follow_Me/human_detection/playground.py
@@ -1,3 +1,4 @@
+''' 
 import cv2
 import pyrealsense2 as rs
 import numpy as np
@@ -56,3 +57,55 @@ finally:
     pipeline.stop()
     cv2.destroyAllWindows()
 
+
+'''
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+
+try:
+    while True:
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+
+        if not depth_frame or not color_frame:
+            continue
+
+        # Convert depth frame to a numpy array
+        depth_image = np.asanyarray(depth_frame.get_data())
+
+        # Normalize the depth values for visualization
+        depth_image = cv2.normalize(depth_image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+
+        # Convert color frame to a numpy array
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Display the depth image
+        cv2.imshow('Depth Image', depth_image)
+
+        # Display the color image
+        cv2.imshow('Color Image', color_image)
+
+        key = cv2.waitKey(1)
+
+        # Exit loop if 'q' is pressed
+        if key == ord('q'):
+            break
+
+finally:
+    # Stop streaming
+    pipeline.stop()
+
+# Close all windows
+cv2.destroyAllWindows()
