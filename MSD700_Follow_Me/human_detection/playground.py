e0d572b773e101a25016cc0b8cbabdd877107273
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-24 15:56:15 +0700

    Add playground

--- MSD700_Follow_Me/human_detection/playground.py
+++ MSD700_Follow_Me/human_detection/playground.py
@@ -2,64 +2,49 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
-class RealsenseCamera:
-    def __init__(self):
-        # Configure depth and color streams
-        print("Loading Intel RealSense Camera")
-        self.pipeline = rs.pipeline()
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-        config = rs.config()
-        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+# Start streaming
+pipeline.start(config)
 
-        # Start streaming
-        self.pipeline.start(config)
-        align_to = rs.stream.color
-        self.align = rs.align(align_to)
-
-
-    def get_frame_stream(self):
+try:
+    while True:
         # Wait for a coherent pair of frames: depth and color
-        frames = self.pipeline.wait_for_frames()
-        aligned_frames = self.align.process(frames)
-        depth_frame = aligned_frames.get_depth_frame()
-        color_frame = aligned_frames.get_color_frame()
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
 
         if not depth_frame or not color_frame:
-            # If there is no frame, probably the camera is not connected, return False
-            print("Error: Unable to get the frame. Make sure that the Intel RealSense camera is correctly connected.")
-            return False, None, None
+            continue
 
-        # Convert images to numpy arrays
+        # Convert depth frame to a numpy array
         depth_image = np.asanyarray(depth_frame.get_data())
+
+        # Normalize the depth values for visualization
+        depth_image = cv2.normalize(depth_image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+
+        # Convert color frame to a numpy array
         color_image = np.asanyarray(color_frame.get_data())
 
-        return True, color_image, depth_image
+        # Display the depth image
+        cv2.imshow('Depth Image', depth_image)
 
-    def release(self):
-        self.pipeline.stop()
+        # Display the color image
+        cv2.imshow('Color Image', color_image)
 
+        key = cv2.waitKey(1)
 
-# Create an instance of the RealsenseCamera class
-camera = RealsenseCamera()
+        # Exit loop if 'q' is pressed
+        if key == ord('q'):
+            break
 
-while True:
-    # Get the color and depth frames from the camera
-    success, color_frame, depth_frame = camera.get_frame_stream()
+finally:
+    # Stop streaming
+    pipeline.stop()
 
-    if not success:
-        break
-
-    # Display the depth image
-    cv2.imshow("Depth Image", depth_frame)
-    cv2.imshow("Color image", color_frame)
-
-    # Press 'q' to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the camera resources
-camera.release()
-
-# Close all OpenCV windows
+# Close all windows
 cv2.destroyAllWindows()
