fe5e7c689db77e0d557c9e90e176857f4624d213
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-24 17:25:25 +0700

    Add playground

--- MSD700_Follow_Me/human_detection/playground.py
+++ MSD700_Follow_Me/human_detection/playground.py
@@ -1,13 +1,13 @@
 import cv2
 import pyrealsense2 as rs
 import numpy as np
-from realsense_camera import *
+#from realsense_camera import *
 
 #realsense = RealsenseCamera()
 
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 pipeline.start()
 
 print("Starting ...")
@@ -15,35 +15,41 @@ tick_frequency = cv2.getTickFrequency()
 start_time = cv2.getTickCount()
 frame_count = 0
 
-while True:
-    # Wait for a coherent pair of frames: color and depth
-    frames = pipeline.wait_for_frames()
-    color_frame = frames.get_color_frame()
+try:
+    while True:
+        # Wait for a coherent pair of frames: color and depth
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
 
-    if not color_frame:
-        continue
+        if not color_frame:
+            continue
 
-    # Convert the color frame to a numpy array
-    color_image = np.asanyarray(color_frame.get_data())
+        # Convert the color frame to a numpy array
+        color_image = np.asanyarray(color_frame.get_data())
 
-    frame_count += 1
-    current_time = cv2.getTickCount()
-    elapsed_time = (current_time - start_time)/tick_frequency
+        frame_count += 1
+        current_time = cv2.getTickCount()
+        elapsed_time = (current_time - start_time)/tick_frequency
 
-    if elapsed_time > 1.0:
-        fps = frame_count / elapsed_time
-        print(fps)
-        start_time = current_time
-        frame_count = 0
+        if elapsed_time > 1.0:
+            fps = frame_count / elapsed_time
+            print(fps)
+            start_time = current_time
+            frame_count = 0
 
-    # Display the resulting frame
-    cv2.imshow('Video Stream', color_image)
+        # Display the resulting frame
+        cv2.imshow('Video Stream', color_image)
 
-    #exit condition
-    key = cv2.waitKey(1)
-    if key == 27:
-        print(f"Key {key} is pressed.")
-        break
+        #exit condition
+        key = cv2.waitKey(1)
+        if key == 27:
+            print(f"Key {key} is pressed.")
+            break
+    
+except Exception as e:
+    print(e)
+    pass
+finally:
+    pipeline.stop()
+    cv2.destroyAllWindows()
 
-pipeline.stop()
-cv2.destroyAllWindows()
