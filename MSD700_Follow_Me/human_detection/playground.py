e9b0a8d11643d001939926743b0bb4b9c7c24115
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-24 17:29:38 +0700

    Add playground

--- MSD700_Follow_Me/human_detection/playground.py
+++ MSD700_Follow_Me/human_detection/playground.py
@@ -7,7 +7,7 @@ import numpy as np
 
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgb8, 30)
 pipeline.start()
 
 print("Starting ...")
