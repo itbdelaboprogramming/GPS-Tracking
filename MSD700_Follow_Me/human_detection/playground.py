ccd70d97da6b48d42a2bb61ca3b74d7d944c0a3c
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-24 17:04:29 +0700

    Add playground

--- MSD700_Follow_Me/human_detection/playground.py
+++ MSD700_Follow_Me/human_detection/playground.py
@@ -1,104 +1,45 @@
+import cv2
 import pyrealsense2 as rs
 import numpy as np
-import cv2
+from realsense_camera import *
 
-# Configure depth and color streams
+realsense = RealsenseCamera()
+
+tick_frequency = cv2.getTickFrequency()
+start_time = cv2.getTickCount()
+frame_count = 0
+
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-# Start streaming
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
 pipeline.start(config)
-align_to = rs.stream.color
-align = rs.align(align_to)
 
-# Create a variable to store the depth scale
-depth_scale = None
+while True:
+    frame = pipeline.wait_for_frames()
+    color_frame = frame.get_color_frame()
+    depth_frame = frame.get_depth_frame()
 
-# Retrieve the depth scale from the pipeline
-profile = pipeline.get_active_profile()
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
+    frame_count += 1
+    current_time = cv2.getTickCount()
+    elapsed_time = (current_time - start_time)/tick_frequency
 
-# Create a variable to store the depth intrinsics
-depth_intrinsics = None
+    if elapsed_time > 1.0:
+        fps = frame_count / elapsed_time
+        print(fps)
+        start_time = current_time
+        frame_count = 0
 
-# Retrieve the depth intrinsics from the pipeline
-depth_stream = profile.get_stream(rs.stream.depth)
-depth_intrinsics = depth_stream.as_video_stream_profile().get_intrinsics()
+    
+    cv2.imshow("BGR", color_frame)
+    cv2.imshow("Depth", depth_frame)
 
-# Create a variable to store the clicked point
-clicked_point = None
+    #exit condition
+    key = cv2.waitKey(1)
+    if key == 27:
+        print(f"Key {key} is pressed.")
+        break
 
-# Mouse callback function
-def mouse_callback(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        global clicked_point
-        clicked_point = (x, y)
-
-# Set the mouse callback function
-cv2.namedWindow('Color Image')
-cv2.setMouseCallback('Color Image', mouse_callback)
-
-try:
-    while True:
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        #depth_frame = frames.get_depth_frame()
-        #color_frame = frames.get_color_frame()
-
-        # Align the depth frame to the color frame
-        #align = rs.align(rs.stream.color)
-        aligned_frames = align.process(frames)
-
-        # Retrieve the aligned depth and color frames
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-
-
-        if not depth_frame or not color_frame:
-            continue
-
-        # Convert depth frame to a numpy array
-        depth_image = np.asanyarray(depth_frame.get_data())
-
-        # Normalize the depth values for visualization
-        depth_image = cv2.normalize(depth_image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-
-        # Convert color frame to a numpy array
-        color_image = np.asanyarray(color_frame.get_data())
-
-        # Display the depth image
-        cv2.imshow('Depth Image', depth_image)
-
-        # Display the color image
-        cv2.imshow('Color Image', color_image)
-
-        # Check if a point has been clicked
-        if clicked_point is not None:
-            # Get the depth value at the clicked point
-            x, y = clicked_point
-            depth_value = depth_frame.get_distance(x, y)
-
-            # Convert the depth value to meters
-            depth_value_meters = depth_value * depth_scale
-
-            # Print the depth value
-            print(f"Depth value at clicked point: {depth_value_meters:.3f} meters")
-
-            # Reset the clicked point
-            clicked_point = None
-
-        key = cv2.waitKey(1)
-
-        # Exit loop if 'q' is pressed
-        if key == ord('q'):
-            break
-
-finally:
-    # Stop streaming
-    pipeline.stop()
-
-# Close all windows
+pipeline.stop()
 cv2.destroyAllWindows()
+#camera.release()
