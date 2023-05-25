141929bb5c76aa8736283aff91d36dba88906835
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-25 16:23:23 +0700

    Adding new device camera

--- MSD700_Follow_Me/human_detection/device_camera_2.py
+++ MSD700_Follow_Me/human_detection/device_camera_2.py
@@ -53,7 +53,7 @@ class DeviceCamera:
         # Configure depth and color streams
         self.pipeline = self.rs.pipeline()
         self.config = self.rs.config()
-        #self.config.enable_stream(self.rs.stream.color, 1280, 720, self.rs.format.bgr8, 30)
+        self.config.enable_stream(self.rs.stream.color, 1280, 720, self.rs.format.bgr8, 30)
         #self.config.enable_stream(self.rs.stream.depth, 1280, 720, self.rs.format.z16, 30)
 
         # Start streaming
