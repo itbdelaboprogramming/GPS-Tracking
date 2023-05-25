3cadd892463527880f3f99354ff51fcd4a77acb5
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-25 17:02:07 +0700

    Adding new device camera

--- MSD700_Follow_Me/human_detection/device_camera_2.py
+++ MSD700_Follow_Me/human_detection/device_camera_2.py
@@ -136,7 +136,7 @@ class DeviceCamera:
         return available_device
 
 def main():
-    camera = DeviceCamera(2, realsense=False)
+    camera = DeviceCamera()
     
     while True:
         color, depth = camera.get_frame()
