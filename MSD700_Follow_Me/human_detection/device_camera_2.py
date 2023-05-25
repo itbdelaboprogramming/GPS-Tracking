f54d3d9905d85f3642b62191a0b6c525b77bbe30
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-25 17:30:22 +0700

    Adding new device camera

--- MSD700_Follow_Me/human_detection/device_camera_2.py
+++ MSD700_Follow_Me/human_detection/device_camera_2.py
@@ -123,7 +123,7 @@ class DeviceCamera:
         elapsed_time = (current_time - self.start_time)/self.tick_frequency
 
         if elapsed_time >= 0.5:
-            self.fps = round(2 * self.frame_count / elapsed_time, 2)
+            self.fps = round(self.frame_count / elapsed_time, 2)
             #print(fps)
             self.start_time = current_time
             self.frame_count = 0
