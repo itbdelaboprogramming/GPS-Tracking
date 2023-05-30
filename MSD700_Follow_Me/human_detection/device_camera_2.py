85251c17f9edc4bbc99990ab505696f4607c7397
luthfihariyadin06 <luthfihariyadin06@gmail.com>
2023-05-30 13:27:14 +0700

    Add minor update

--- MSD700_Follow_Me/human_detection/device_camera_2.py
+++ MSD700_Follow_Me/human_detection/device_camera_2.py
@@ -41,7 +41,7 @@ class DeviceCamera:
         self.org = (0, 50)
         self.font_scale = 0.5
         self.font_color = (90, 252, 3)
-        self.font_thickness = 2
+        self.font_thickness = 1
         self.font_line_type = cv2.LINE_AA
         self.font_bottom_left_origin = False
     
@@ -116,8 +116,8 @@ class DeviceCamera:
             self.start_time = current_time
             self.frame_count = 0
 
-        #text_size, _ = cv2.getTextSize(f"FPS: {self.fps}", self.font_face, self.font_scale)
-        cv2.rectangle(frame,self.org,(200, 100), (255,255,255), cv2.FILLED)
+        text_size, _ = cv2.getTextSize(f"FPS: {self.fps}", self.font_face, self.font_scale, self.font_thickness)
+        cv2.rectangle(frame,self.org, (self.org[0] + text_size[0], self.org[1] + text_size[1]), (255,255,255), cv2.FILLED)
         cv2.putText(frame, f"FPS: {self.fps}", self.org, self.font_face, self.font_scale, self.font_color, self.font_thickness, self.font_line_type, self.font_bottom_left_origin)
         return frame
 
